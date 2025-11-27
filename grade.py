cbc26126de9dbd4aca2f556693055798932daafc
veereshsajjansajjan1-svg <veereshsajjansajjan1@gmail.com>
2025-11-27 14:22:42 +0530

    Update grade.py

--- grade.py
+++ grade.py
@@ -4,10 +4,14 @@ if len(sys.argv) != 6:
     sys.exit(1)
 marks = [float(arg) for arg in sys.argv[1:6]]
 average = sum(marks) / 5
-if average >= 90: grade = "A"
-elif average >= 75: grade = "B"
-elif average >= 60:grade = "C"
-elif average >= 40: grade = "D"
+if average >= 90: 
+    grade = "A"
+elif average >= 75: 
+    grade = "B"
+elif average >= 60:
+    grade = "C"
+elif average >= 40:
+    grade = "D"
 else:
     grade = "Fail"
 print("\n- Result -")
